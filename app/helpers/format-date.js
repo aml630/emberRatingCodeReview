42d9c57fde131981f91fab6165916a3c9212a080
alex <alex.m.larson@gmail.com>
2016-04-11 17:01:21 -0700

    add attempt at sort

---
+++ app/helpers/format-date.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+// import moment from 'moment';
+
+
+// export function formatDate(params/*, hash*/) {
+//   console.log(params);
+//   var newDate = moment().format(params)
+//   return newDate;
+// }
+
+export default Ember.Helper.helper(formatDate);
--- app/routes/business.js
+++ app/routes/business.js
@@ -1,17 +1,22 @@
 import Ember from 'ember';
+// import moment from 'moment';
+
 
 export default Ember.Route.extend({
-  
+sortBy: ['stars.asc'],
+sortedRatings: Ember.computed.sort('business.ratings', 'sortBy'),
+
   model(business) {
     return this.store.findRecord('business', business.business_id);
   },
   actions: {
     saveRating(reviewParams) {
       var newRating = this.store.createRecord('rating', reviewParams);
-      console.log(newRating);
+    
       var business = reviewParams.business;
       business.get('ratings').addObject(newRating);
       newRating.save().then(function () {
+        console.log("sortedRatings "+ sortedRatings);
         return business.save();
       });
   }
