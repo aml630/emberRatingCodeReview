33b90dfd27b7abb4ea8044abb1d4ef831797637a
austinminnon <whi7ee@gmail.com>
2016-04-13 17:20:23 -0700

    added fav list printout functionality

--- app/components/new-business.js
+++ app/components/new-business.js
@@ -6,9 +6,17 @@ export default Ember.Component.extend({
   favoriteCount: Ember.computed('favoriteBiz.businesses.length', function () {
     return this.get('favoriteBiz').businesses.length;
   }),
-  favoriteList: Ember.computed('favoriteBiz.businesses', function () {
-    console.log("Fav list log "  + this.get('favoriteBiz.businesses'));
-  }),
+  favoriteList: Ember.computed('favoriteBiz.businesses.length', function () {
+    var words = "";
+    var list = this.get('favoriteBiz').businesses;
+    console.log(list);
+    for(var i = 0; i<list.length;i++){
+      words+= list[i]._internalModel._data.name + ", "
+
+    }
+
+    return words
+    }),
 
   actions: {
     savebiz() {
